45dcd567449b4a2ddc2922f54e1e8af283115169
Fiesty-Cushion <neupane.braz07@gmail.com>
2022-09-30 14:02:08 +0545

    Assigned id to all districts

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,5 +1,97 @@
-document.querySelector('#states').addEventListener('click', action)
 
-function action(){
-    alert("Heyo")
+let allDistricts = [
+    "Achham",
+    "Arghakhanchi",
+    "Baglung",
+    "Baitadi",
+    "Bajhang",
+    "Bajura",
+    "Banke",
+    "Bara",
+    "Bardiya",
+    "Bhaktapur",
+    "Bhojpur",
+    "Chitwan",
+    "Dadeldhura",
+    "Dailekh",
+    "Dang",
+    "Darchula",
+    "Dhading",
+    "Dhankuta",
+    "Dhanusa",
+    "Dholkha",
+    "Dolpa",
+    "Doti",
+    "Gorkha",
+    "Gulmi",
+    "Humla",
+    "Ilam",
+    "Jajarkot",
+    "Jhapa",
+    "Jumla",
+    "Kailali",
+    "Kalikot",
+    "Kanchanpur",
+    "Kapilvastu",
+    "Kaski",
+    "Kathmandu",
+    "Kavrepalanchok",
+    "Khotang",
+    "Lalitpur",
+    "Lamjung",
+    "Mahottari",
+    "Makwanpur",
+    "Manang",
+    "Morang",
+    "Mugu",
+    "Mustang",
+    "Myagdi",
+    "Nawalparasi East",
+    "Nawalparasi West",
+    "Nuwakot",
+    "Okhaldhunga",
+    "Palpa",
+    "Panchthar",
+    "Parbat",
+    "Parsa",
+    "Pyuthan",
+    "Ramechhap",
+    "Rasuwa",
+    "Rautahat",
+    "Rolpa",
+    "Rukum East",
+    "Rukum West",
+    "Rupandehi",
+    "Salyan",
+    "Sankhuwasabha",
+    "Saptari",
+    "Sarlahi",
+    "Sindhuli",
+    "Sindhupalchok",
+    "Siraha",
+    "Solukhumbu",
+    "Sunsari",
+    "Surkhet",
+    "Syangja",
+    "Tanahu",
+    "Taplejung",
+    "Terhathum",
+    "Udayapur"
+]
+
+let someDistricts = allDistricts
+
+
+function randomDistrict(districts){
+    let randint = Math.random()
+    index = randint * districts.length
+    element = Math.floor(index)
+    return (districts[element])
 }
+
+
+function displayDistrict(){
+    document.querySelector('h1').innerText = randomDistrict(someDistricts)
+}
+
+displayDistrict()
