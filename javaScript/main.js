9c4e7a714aa8a2994bd68726bde74b7514244109
Fiesty-Cushion <neupane.braz07@gmail.com>
2022-10-05 10:33:42 +0545

    working on next and previous buttons

--- javaScript/main.js
+++ javaScript/main.js
@@ -80,8 +80,9 @@ let allDistricts = [
 ]
 
 let remDistricts = allDistricts
+let storesRandomDistricts = []
 
-let score = 0, botChoice, element
+let streak = 0, score = 0, botChoice, element, indexSRD = 0
 let highScore = localStorage.getItem('highScore')
 checkHighScore()
 
@@ -91,6 +92,9 @@ function randomDistrict(){
     element = Math.floor(index)
     botChoice = remDistricts[element]
 
+    storesRandomDistricts[indexSRD] = botChoice
+    indexSRD++
+
     document.querySelector("#display").innerHTML = `Where is :  ${botChoice}?`
 }
 
@@ -99,6 +103,7 @@ randomDistrict()
 function check(userChoice){
     
     if(userChoice == botChoice){
+        streak++
         score++
         checkHighScore()
         document.querySelector('#score').innerHTML = `Score : ${score}`
@@ -108,16 +113,27 @@ function check(userChoice){
         document.getElementById(userChoice).removeEventListener("click", districtSelected)
 
         randomDistrict()
+        
+        if(streak == 77){
+            finalScore()
+        }
+
     }
     
     else{
+        streak++
         document.getElementById(userChoice).style.fill =  "	#D2042D"
         remDistricts.splice(element, 1)
         document.getElementById(userChoice).removeEventListener("click", districtSelected)
 
         randomDistrict()
+
+        if(streak == 77){
+            finalScore()
+        }
     }
 }
+
 
 const districts = document.getElementsByTagName("polygon");
 
@@ -133,13 +149,7 @@ for (let polygon of districts) {
 
 }
 
-document.querySelector('button').addEventListener("click", playAgain)
 
-
-function playAgain(){
-    window.location.reload()
-    
-}
 
 function checkHighScore(){
 
@@ -154,4 +164,29 @@ function checkHighScore(){
     document.querySelector('#pb').innerHTML = `High Score : ${highScore}`
     
 }
+
+
+function finalScore(){
+    alert(`You scored ${score} out of 77!`)
+
+}
+
+document.querySelector('#again').addEventListener("click", playAgain)
+document.querySelector('#prev').addEventListener("click", previousDistrict)
+document.querySelector('#next').addEventListener("click", nextDistrict)
+
+
+function playAgain(){
+    window.location.reload()
+    
+}
+
+function previousDistrict(){
+
+}
+
+function nextDistrict(){
+
+}
+
 
